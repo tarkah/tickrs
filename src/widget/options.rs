2fe6bba09f8da02f29a6987a51d3485b9c7443ce
Cory Forsstrom <cforsstrom18@gmail.com>
2020-08-22 10:30:30 -0700

    fix panic on index error if no options for stock

--- src/widget/options.rs
+++ src/widget/options.rs
@@ -175,7 +175,7 @@ impl OptionsState {
 
                     self.exp_dates = dates;
 
-                    if prev_len == 0 {
+                    if prev_len == 0 && !self.exp_dates.is_empty() {
                         self.set_exp_date(self.exp_dates[0]);
                     }
                 }
